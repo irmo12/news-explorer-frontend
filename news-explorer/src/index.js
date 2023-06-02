460719b074a6763bee65cf54558c4cd1376ca3b5
irmo12 <omriws@hushmail.com>
2023-06-02 17:41:31 +0300

    import app fix

--- news-explorer/src/index.js
+++ news-explorer/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import App from './components/App/App.js';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
