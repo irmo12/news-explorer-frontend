b93b6dc75940e97e0247c67a6b63c46890a35838
irmo12 <omriws@hushmail.com>
2023-06-02 12:07:50 +0300

    font-faces

--- news-explorer/src/index.js
+++ news-explorer/src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>News Explorer</title>
+  <meta
+    name="description"
+    content="Your news catalogue where you may search for articles and save them for furture reference."
+  />
+  <meta
+    name="keywords"
+    content="web development, JS web development, website development project, web development education"
+  />
+  <meta name="author" content="Omri Ben Tal at Practicum" />
+  <link rel="favicon" href="./favicon.ico" />
+</head>;
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
