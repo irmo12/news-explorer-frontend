6eaa44908ee44b5419ff1f35c09395c27c4f7383
irmo12 <omriws@hushmail.com>
2023-06-10 16:50:26 +0300

    stlyes WIP

--- news-explorer/src/components/Main/Main.js
+++ news-explorer/src/components/Main/Main.js
@@ -3,18 +3,21 @@ import './Main.css';
 import Header from '../Header/Header.js';
 import About from '../About/About.js';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader.js';
 
 
 function Main() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasResults, setHasResults] = useState(false);
 
   return (
     <>
       <div className="main">
-        <div className="main__header+search-container">
-      <Header />
-      <SearchForm />
-      </div>
-
+        <div className="main__header-search-container">
+          <Header />
+          <SearchForm />
+        </div>
+        <Preloader />
         <About />
 
 
--- news-explorer/src/components/Preloader/Preloader.js
+++ news-explorer/src/components/Preloader/Preloader.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './Preloader.css';
 import magnifyingGlass from '../../images/not-found_v1.svg';
 
-const Preloader = () => {
+const Preloader = ({ isLoading = true, hasResults = false }) => {
+
   return (
     <div className="preloader">
-      <i class="circle-preloader"></i> : <image src={magnifyingGlass} alt='magnifying glass' />
-      <h3 className="preloader__title">Nothing Found</h3>
-      <p className='preloader__description'>Searching for news...  -----  Sorry, but nothing matched your search terms</p>
+      <div className="preloader-container">
+      {hasResults ? <i class="circle-preloader"></i> : <image src={magnifyingGlass} alt='magnifying glass' />}
+      <h3 className="preloader__title">{hasResults ? '' : 'Nothing Found'}</h3>
+      <p className='preloader__description'>{hasResults ? 'Searching for news...' : 'Sorry, but nothing matched your search terms'}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default Preloader;
