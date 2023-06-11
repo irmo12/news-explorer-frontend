96308af7148eaf6c87bef009b7681b053ddce3a9
irmo12 <omriws@hushmail.com>
2023-06-11 15:39:48 +0300

    style footer, search

--- news-explorer/src/components/Footer/Footer.js
+++ news-explorer/src/components/Footer/Footer.js
@@ -1,19 +1,22 @@
-import React, { Link } from "react";
+import React from "react";
+import { Link } from 'react-router-dom';
 import './Footer.css';
 import fblogo from '../../images/fb.svg';
 import github from '../../images/github.svg';
 
-function Footer(props) {
+function Footer() {
   return (
     <div className="footer">
       <div className='footer-signature'>
         © {new Date().getFullYear()} Supersite, Powered by News API
       </div>
       <div className='footer-links'>
-        {/* <Link to={'/'} className='footer-link'>Home</Link> */}
+        <Link to={'/'} className='footer-link'>Home</Link>
         <a href="www.tripleten.com" className='footer-link'>TripleTen</a>
-        <a href="#" className='footer-link'><img src={github} alt='github' /></a>
-        <a href="#" className='footer-link'><img src={fblogo} alt='facebook' /></a>
+        <div className='footer-links__icons'>
+          <a href="#" className='footer-link'><img src={github} alt='github' /></a>
+          <a href="#" className='footer-link'><img src={fblogo} alt='facebook' /></a>
+        </div>
       </div>
     </div>
   );
