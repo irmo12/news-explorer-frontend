6982a3c940ed44f5549898f962e4cc3bfd1f7e12
irmo12 <omriws@hushmail.com>
2023-06-11 15:16:02 +0300

    done preloader

--- news-explorer/src/components/Preloader/Preloader.js
+++ news-explorer/src/components/Preloader/Preloader.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Preloader.css';
 import magnifyingGlass from '../../images/not-found_v1.svg';
 
-const Preloader = ({ isLoading = true, hasResults = true }) => {
+const Preloader = ({ isLoading = true, hasResults = false }) => {
 
   return (
     <div className="preloader">
       <div className="preloader-container">
       {hasResults ? <i class="circle-preloader"></i> : <embed className='preloader__img' src={magnifyingGlass} alt='magnifying glass' />}
-      <h3 className={ hasResults ? 'preloader__title preloader__title_none' : 'preloader__title'} >Nothing Found'</h3>
+      <h3 className={ hasResults ? 'preloader__title preloader__title_none' : 'preloader__title'} >Nothing Found</h3>
       <p className='preloader__description'>{hasResults ? 'Searching for news...' : 'Sorry, but nothing matched your search terms'}</p>
       </div>
     </div>
