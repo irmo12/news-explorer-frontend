cce881da191b0d9701779719db03f431ada9a008
irmo12 <omriws@hushmail.com>
2023-09-02 17:43:25 +0300

    vis fixes grid

--- news-explorer/src/components/NewsCardList/NewsCardList.js
+++ news-explorer/src/components/NewsCardList/NewsCardList.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import './NewsCardList.css'; 
+import React, { useState } from 'react';
+import './NewsCardList.css';
 import NewsCard from '../NewsCard/NewsCard';
 
-function NewsCardList( {articleList} ) {
+function NewsCardList({ articleList }) {
+  const [showAll, setShowAll] = useState(false);
+
+  const visibleCards = showAll ? articleList.length : 3;
+
+  function handleShowMoreClick() {
+    setShowAll(!showAll);
+  }
+
   return (
     <div className="news-card-list">
       <h2 className='news-card-list__heading'>Search results</h2>
-      <div className="news-card-list__grid">
-      {articleList.map((article) => (
+      <div className='news-card-list__grid'>
+        {articleList.slice(0, visibleCards).map((article) => (
           <NewsCard article={article} key={article._id} />
         ))}
       </div>
-      <button className='news-card-list__button'>Show more</button>
+      {articleList.length > 3 && (
+        <button className='news-card-list__button' onClick={handleShowMoreClick}>
+          {showAll ? 'Show Less' : 'Show More'}
+        </button>
+      )}
     </div>
   );
 }
